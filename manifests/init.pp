cdc07fa15410766042ec96651f30f61b2e1a5be6
Choi, Sunghee <Sunghee.Choi@cdk.com>
2016-02-04 14:00:13 -0600

    respacing files.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,22 @@
 class nxlog (
-    $package_ensure             =   present,
-    $package_name               =   "NXLOG-CE",
-    $package_version            =   "2.8.1248",
-    $install_dir                =   "C:\\Program Files (x86)\\nxlog\\",
-    $package_src                =   "\\path\\to\\nxlog-ce-2.8.1248.msi", # unc
-    $package_src_http           =   undef, # if defined, HTTP, ftp, local, s3
-    $service_name               =   "nxlog",
-    $service_ensure             =   running,
-    $config_dir                 =   "C:\\Program Files (x86)\\nxlog\\conf\\",
-    $config_file                =   "nxlog.conf",
-    $temp_media_dir             =   'C:\media',
-    $include_external_configs   =   false,
-    $external_config_path       =   "C:\\nxlog\\configuration\\*.nxlog.conf",
-    $nxlog_moduledir            =   "C:\\Program Files (x86)\\nxlog\\modules",
-    $nxlog_cachedir             =   "C:\\Program Files (x86)\\nxlog\\data",
-    $nxlog_pidfile              =   "C:\\Program Files (x86)\\nxlog\\data\\nxlog.pid",
-    $nxlog_spooldir             =   "C:\\Program Files (x86)\\nxlog\\data",
-    $nxlog_logfile              =   "C:\\Program Files (x86)\\nxlog\\data\\nxlog.log",
+  $package_ensure             =   present,
+  $package_name               =   "NXLOG-CE",
+  $package_version            =   "2.8.1248",
+  $install_dir                =   "C:\\Program Files (x86)\\nxlog\\",
+  $package_src                =   "\\path\\to\\nxlog-ce-2.8.1248.msi", # unc
+  $package_src_http           =   undef, # if defined, HTTP, ftp, local, s3
+  $service_name               =   "nxlog",
+  $service_ensure             =   running,
+  $config_dir                 =   "C:\\Program Files (x86)\\nxlog\\conf\\",
+  $config_file                =   "nxlog.conf",
+  $temp_media_dir             =   'C:\media',
+  $include_external_configs   =   false,
+  $external_config_path       =   "C:\\nxlog\\configuration\\*.nxlog.conf",
+  $nxlog_moduledir            =   "C:\\Program Files (x86)\\nxlog\\modules",
+  $nxlog_cachedir             =   "C:\\Program Files (x86)\\nxlog\\data",
+  $nxlog_pidfile              =   "C:\\Program Files (x86)\\nxlog\\data\\nxlog.pid",
+  $nxlog_spooldir             =   "C:\\Program Files (x86)\\nxlog\\data",
+  $nxlog_logfile              =   "C:\\Program Files (x86)\\nxlog\\data\\nxlog.log",
 )
 {
   include staging
@@ -25,72 +25,67 @@ class nxlog (
 
   $local_package_msi = "${src_dir}\\${package_name}-${package_version}.msi"
 
-    if "${::operatingsystem}" == 'windows' {
+  if "${::operatingsystem}" == 'windows' {
 
-        Exec {
-            path => "${::path}",
+    Exec {
+      path => "${::path}",
+    }
+
+    if $package_ensure == 'installed' or $package_ensure == 'present' {
+
+      package { $package_name:
+        ensure  => $package_ensure,
+        source  => $local_package_msi,
+        require => File["${local_package_msi}"],
+      }
+
+      file { "${config_dir}${config_file}":
+        ensure  => present,
+        content => regsubst(template('nxlog/nxlog.conf.erb'), '\n', "\r\n", 'EMG'),
+        require => Package["${package_name}"],
+      }
+
+      service { $service_name:
+        ensure  => $service_ensure,
+        require => File["${config_dir}${config_file}"],
+      }
+
+      if ! defined(File["${temp_media_dir}"]) {
+        file { "${temp_media_dir}":
+          ensure=>directory,
         }
+      }
+      if ! defined(File["${src_dir}"]){
+        file { "${src_dir}":
+          ensure => directory,
+        }
+      }
+      if $package_src_http == undef {
+        file { "${local_package_msi}" :
+          ensure => 'file',
+          source => "${package_src}",
+          replace => false,
+          require => File[$src_dir],
+        }
+      }
+      else{
+        staging::file { "${package_name}-${package_version}.msi":
+          source  => "${package_src_http}",
+        }
+        ->
+        file { "${local_package_msi}":
+          ensure => 'file',
+          source => "${staging::path}/nxlog/${package_name}-${package_version}.msi",
+          replace => false,
+          require => File[$src_dir],
+        }
+      }
 
-        if $package_ensure == 'installed' or $package_ensure == 'present' {
-
-
-
-            package { $package_name:
-                ensure  => $package_ensure,
-                source  => $local_package_msi,
-                require => File["${local_package_msi}"],
-            }
-
-            file { "${config_dir}${config_file}":
-                ensure  => present,
-                content => regsubst(template('nxlog/nxlog.conf.erb'), '\n', "\r\n", 'EMG'),
-                require => Package["${package_name}"],
-            }
-
-            service { $service_name:
-                ensure  => $service_ensure,
-                require => File["${config_dir}${config_file}"],
-            }
-
-            if ! defined(File["${temp_media_dir}"]) {
-                file { "${temp_media_dir}":
-                    ensure=>directory,
-                }
-            }
-          if ! defined(File["${src_dir}"]){
-            file { "${src_dir}":
-              ensure => directory,
-            }
-          }
-            if $package_src_http == undef {
-               file { "${local_package_msi}" :
-                    ensure => 'file',
-                    source => "${package_src}",
-                    replace => false,
-                    require => File[$src_dir],
-               }
-            }
-            else{
-                staging::file { "${package_name}-${package_version}.msi":
-                    source  => "${package_src_http}",
-                }
-                ->
-                file { "${local_package_msi}":
-                  ensure => 'file',
-                  source => "${staging::path}/nxlog/${package_name}-${package_version}.msi",
-                  replace => false,
-                  require => File[$src_dir],
-                }
-            }
-
-
-
-
-        } # if $package_ensure == 'installed' or $package_ensure == 'present'
-    }# if "${::operatingsystem}" == 'windows'
-    else{
-        notice("NXLOG: Not supported on non-windows platforms at this time.")
-    }# else of (if "${::operatingsystem}" == 'windows')
+    } # if $package_ensure == 'installed' or $package_ensure == 'present'
+  }# if "${::operatingsystem}" == 'windows'
+  else{
+    notice("NXLOG: Not supported on non-windows platforms at this time.")
+  }# else of (if "${::operatingsystem}" == 'windows')
 
 
 }
